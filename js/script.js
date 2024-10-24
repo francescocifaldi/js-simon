dbd17c89cea6b084225dab2c5cee29f0148af5b4
francescocifaldi <francesco.cifaldi@gmail.com>
2024-10-24 14:50:59 +0200

    ADD countdown to fix

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 const arrayLength = 5;
 const max = 50;
+const randomArray = document.getElementById('numbers-list');
+const countdown = document.getElementById('countdown');
+const countdownStart = 10;
+let tempoRimasto = countdownStart;
+
+const countdownID = setInterval(function (){
+    countdown.innerHTML=tempoRimasto;
+    -- tempoRimasto
+}, 1000)
+
+setTimeout(function(){
+    clearInterval(countdownID)
+}, (countdownStart*1000))
 
 function generateRandomArray (arrayLength){
     let array = []
