54f782f1254f44b7cfa937512bcd897cda62fc0e
francescocifaldi <francesco.cifaldi@gmail.com>
2024-10-24 16:10:43 +0200

    ADD result print

--- js/script.js
+++ js/script.js
@@ -1,26 +1,64 @@
+//init all variables to handle DOM
+
 const arrayLength = 5;
 const max = 50;
-const randomArray = document.getElementById('numbers-list');
+const numbersList = document.getElementById('numbers-list');
+const answerForm = document.getElementById('answers-form')
 const countdown = document.getElementById('countdown');
-const countdownStart = 10;
+const instructionElement = document.getElementById('instructions');
+const instructionAfterElement = document.getElementById('instructionsAfter')
+const myBtn = document.getElementById('btn');
+const resultMessage = document.getElementById('message')
+const refreshButton = document.getElementById('refresh')
+const countdownStart = 2;
 const intervalInMs = 1000;
 let tempoRimasto = countdownStart;
+const array = generateRandomArray(arrayLength);
+let copyArray = array.slice();
 
-countdown.innerHTML=tempoRimasto;
+countdown.innerHTML = tempoRimasto;
 
-const countdownID = setInterval(function (){
-    -- tempoRimasto
-    countdown.innerHTML=tempoRimasto;
+//start countdown from variable
+const countdownID = setInterval(function () {
+    --tempoRimasto
+    countdown.innerHTML = tempoRimasto;
 }, intervalInMs)
 
-setTimeout(function(){
+//exit from interval and change visibility of html elements
+setTimeout(function () {
     clearInterval(countdownID)
-}, (countdownStart*intervalInMs))
+    countdown.classList.add('d-none')
+    instructionElement.classList.add('d-none')
+    numbersList.classList.add('d-none')
+    instructionAfterElement.classList.remove('d-none')
+    answerForm.classList.remove('d-none')
+}, (countdownStart * intervalInMs))
 
-function generateRandomArray (arrayLength){
+//when user clicks, take all the answers and check with random array then print the result 
+myBtn.addEventListener('click', function (event) {
+    event.preventDefault();
+    let answerList = document.querySelectorAll('input')
+    let correctAnswer = [];
+    for (let i = 0; i < answerList.length; i++) {
+        if (copyArray.includes(parseInt(answerList[i].value))) {
+            correctAnswer.push(answerList[i].value)
+            let temp = array.indexOf(answerList[i].value)
+            copyArray.shift(temp);
+        }
+    }
+    instructionAfterElement.classList.add('d-none')
+    answerForm.classList.add('d-none')
+    resultMessage.innerHTML = `hai indovinato ${correctAnswer.length} corrispondenze`
+    if (correctAnswer.length > 0) {
+        resultMessage.innerHTML += ` ovvero: ${correctAnswer}`
+    }
+})
+
+function generateRandomArray(arrayLength) {
     let array = []
-    for(let i =0;i<(arrayLength);i++){
-        array[i]=getRandomInt(max);
+    for (let i = 0; i < (arrayLength); i++) {
+        array[i] = getRandomInt(max);
+        numbersList.innerHTML += `<li>${array[i]}</li>`
     }
     return array;
 }
@@ -28,5 +66,3 @@ function generateRandomArray (arrayLength){
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
-
-console.log(generateRandomArray(arrayLength));
